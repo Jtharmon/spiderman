66fec547c3a14faf58fe951bb8044a68e9a0a101
Jtharmon <Jtharmon10@gmail.com>
2025-02-08 14:22:08 -0500

    theme switching still in progress building progress with content coming and going

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
-const theme1Content = document.querySelector('.theme1-content');
-const theme2Content = document.querySelector('.theme2-content');
+const peterParkerContent = document.getElementById('peterParkerContent');
 
 themeToggle.addEventListener('change', () => {
   if (themeToggle.checked) {
     body.classList.remove('theme1');
     body.classList.add('theme2');
-    theme1Content.classList.add('hidden');
-    theme2Content.classList.remove('hidden');
+    peterParkerContent.classList.remove('hidden');
   } else {
     body.classList.remove('theme2');
     body.classList.add('theme1');
-    theme2Content.classList.add('hidden');
-    theme1Content.classList.remove('hidden');
+    peterParkerContent.classList.add('hidden');
   }
 });
